e3558e3a1e3610bcdbb5d4cd66e494d656188889
Obony <faithnyabz69@gmail.com>
2023-03-15 04:35:19 -0700

    function that replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,12 +3,4 @@
 #Faith Nyaberi <faithnyabz69@gmail.com>
 
 def search_replace(my_list, search, replace):
-    '''Replace all occurrences of an element by another in a new list.'''
-    copy = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            copy.append(replace)
-        else:
-            copy.append(my_list[i])
-            return copy
-
+    return (list(map(lambda x: replace if x is search else x, my_list)))
