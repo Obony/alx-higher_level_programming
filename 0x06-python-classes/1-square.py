d65022deda1e120eb130cf884fc26581e202f720
Obony <faithnyabz69@gmail.com>
2023-03-28 05:29:05 -0700

    Define square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Declare a square"""
+
+class Square:
+    """Represent a square"""
+
+    def ___init__(self, size):
+
+        """Initialize a square
+        Args:
+            size: size of square
+        """
+
+        self.__size = size
