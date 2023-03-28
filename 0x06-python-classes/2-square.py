b134f92584e1eab57f393fe017c833742241c288
Obony <faithnyabz69@gmail.com>
2023-03-28 06:07:28 -0700

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,4 +14,5 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
