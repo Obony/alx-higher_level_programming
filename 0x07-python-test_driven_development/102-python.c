abeff723785babfce1450d8c8a408a18b1e8eb84
Obony <faithnyabz69@gmail.com>
2023-04-03 07:40:03 -0700

    corrections

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,8 +1,10 @@
 #include "Python.h"
+
 /**
  * print_python_string - Prints information about Python strings.
  * @p: A PyObject string object.
  */
+
 void print_python_string(PyObject *p)
 {
 	long int length;
