3bd923e672f96c6e4656b99f8e320e47e6160c10
Obony <faithnyabz69@gmail.com>
2023-04-11 14:57:49 -0700

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+    Implement Geometry class
+'''
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    '''
+        Implement class Square
+    '''
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
