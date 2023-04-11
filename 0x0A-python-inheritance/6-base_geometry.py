fdcfc29b2fd399a6668e7549be9ac06ce467ef80
Obony <faithnyabz69@gmail.com>
2023-04-11 14:05:13 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""creating BaseGeometry class"""
+
+
+class BaseGeometry:
+    """class BaseGeometry represented"""
+
+    def area(self):
+        """when it is not implemented"""
+
+        raise Exception("area() is not implemented")
