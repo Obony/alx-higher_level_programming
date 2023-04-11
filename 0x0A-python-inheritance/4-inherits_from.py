2bfb8f709af48ebbb329a99d44dd99f47dd037a5
Obony <faithnyabz69@gmail.com>
2023-04-11 13:51:02 -0700

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""Defines an inherited class-checking function."""
+
+
 def inherits_from(obj, a_class):
-    '''
-        Returns True if the object is an instance of a class that inherited
-        (directly or indirectly) from the specified class ; otherwise False
-    '''
-    return (issubclass(type(obj), a_class) and type(obj) != a_class)
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
