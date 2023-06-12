4619b53c7b671558676616e3cfb70b6c0c6dd09f
Obony <faithnyabz69@gmail.com>
2023-06-12 05:19:13 -0700

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const num = process.argv[2];
-if (!isNaN(parseInt(process.argv[2]))) {
+const num = Math.floor(Number(process.argv[2]));
+if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
