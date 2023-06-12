4822614b4ff3fa97c1a741d6dd5d936e299763ff
Obony <faithnyabz69@gmail.com>
2023-06-12 05:50:03 -0700

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -7,6 +7,4 @@ const num2 = parseInt(process.argv[3]);
 if (!isNaN(num1) && !isNaN(num2)) {
   const result = add(num1, num2);
   console.log(result);
-} else {
-  console.log('Missing or invalid input');
 }
