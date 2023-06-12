1c01e4c66a62f5654856237c45c7ca5574fe144a
Obony <faithnyabz69@gmail.com>
2023-06-12 04:27:24 -0700

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < lines.length) {
+  console.log(lines[i]);
+  i++;
 }
+
