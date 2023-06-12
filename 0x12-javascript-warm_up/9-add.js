00b2541d77446a5547eb9e58d2f3eecd457819bb
Obony <faithnyabz69@gmail.com>
2023-06-12 05:55:14 -0700

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 function add (a, b) {
-  return a + b;
+  console.log(parseInt(a) + parseInt(b));
 }
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-if (!isNaN(num1) && !isNaN(num2)) {
-  const result = add(num1, num2);
-  console.log(result);
-}
+add(process.argv[2], process.argv[3]);
