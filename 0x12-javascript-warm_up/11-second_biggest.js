bee7ccff28f8d5b07885c41707848045bf5d06dc
Obony <faithnyabz69@gmail.com>
2023-06-12 06:45:07 -0700

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,23 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+const args = process.argv.slice(2).map(Number);
+if (args.length <= 1) {
   console.log(0);
 } else {
-  const args = process.argv.map(parseInt)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
+  let max = -Infinity;
+  let secondMax = -Infinity;
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > max) {
+      secondMax = max;
+      max = args[i];
+    } else if (args[i] > secondMax && args[i] < max) {
+      secondMax = args[i];
+    }
+  }
+
+  if (secondMax === -Infinity) {
+    console.log(0);
+  } else {
+    console.log(secondMax);
+  }
 }
