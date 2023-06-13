14fc88bd1419463381db0e417075a27ac1b295ad
Obony <faithnyabz69@gmail.com>
2023-06-13 03:21:57 -0700

    Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w,h) {
+    if (w > 0 && h > 0) {
+      this.w = width;
+      this.h = height;
+    } else {
+    }
+  }
+}
+module.exports = Rectangle;
