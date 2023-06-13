a004815bc67c463beea2f9e8100837bdad045441
Obony <faithnyabz69@gmail.com>
2023-06-13 05:38:31 -0700

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,21 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square.js') {
+class Square extends require('./5-square.js') {
   charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    if (!c) {
+      c = 'X';
+    }
+    if (!this.width || !this.height) {
+      return;
+    }
+
+    let x = c;
+    for (let i = 1; i < this.width; i++) {
+      x += c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(x);
     }
   }
-};
+}
+
+module.exports = Square;
