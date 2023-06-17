8b9c2e6ee541b8cc2bc010f5d4f32420d5b1cf92
Obony <faithnyabz69@gmail.com>
2023-06-17 04:22:58 -0700

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,8 +15,8 @@ def main():
     from model_state import Base, State
 
     connection = "mysql+mysqldb://{}:{}@localhost/{}".format(str(argv[1]),
-                                                      str(argv[2]),
-                                                      str(argv[3]))
+                                                             str(argv[2]),
+                                                             str(argv[3]))
     engine = create_engine(connection)
     Session = sessionmaker(bind=engine)
     session = Session()
