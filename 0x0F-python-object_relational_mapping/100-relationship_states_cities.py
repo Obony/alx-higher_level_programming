58777e0de550c29ee2771d6112ae8b113e727602
Obony <faithnyabz69@gmail.com>
2023-06-17 06:12:11 -0700

    City relationship***

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
-"""
-This script a state and its city
-"""
+# Creates the State “California” with the City “San Francisco”
+# from the database hbtn_0e_100_usa.
+# Usage: ./100-relationship_states_cities.py <mysql username> /
+#                                            <mysql password> /
+#                                            <database name>
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import Base, City
 
-
-def main():
-    """
-    Create relationship between newly created
-    'California' state and newly created 'San Francisco' city
-    """
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from sys import argv
-
-    from relationship_state import Base, State
-    from relationship_city import City
-
-    connection = "mysql+mysqldb://{}:{}@localhost/{}".format(str(argv[1]),
-                                                             str(argv[2]),
-                                                             str(argv[3]))
-
-    engine = create_engine(connection)
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_city = City(name="San Francisco")
-    new_state = State(name="California")
-    new_state.cities.append(new_city)
-    session.add_all([new_state, new_city])
+    session.add(City(name="San Francisco", state=State(name="California")))
     session.commit()
-    session.close()
-
-
-if __name__ == '__main__':
-    main()
