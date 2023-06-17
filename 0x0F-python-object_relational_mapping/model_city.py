ed5821e2681ea0d69e3c448be2747e7f3ce76633
Obony <faithnyabz69@gmail.com>
2023-06-17 05:19:00 -0700

    Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,8 +12,6 @@ class City(Base):
         Creates table for cities
     '''
     __tablename__ = "cities"
-    id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-    state = relationship("State", back_populates="cities")
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
