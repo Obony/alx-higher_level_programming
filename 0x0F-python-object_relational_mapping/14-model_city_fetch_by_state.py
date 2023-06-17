abbee4bdcfcc9d2c66a7ad7521cf195d073eacb2
Obony <faithnyabz69@gmail.com>
2023-06-17 05:23:54 -0700

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-"""
-This module lists all states from 'hbtn_0e_6_usa' database
-"""
+# Lists all City objects from the database hbtn_0e_14_usa.
+# Usage: ./14-model_city_fetch_by_state.py <mysql username> /
+#                                          <mysql password> /
+#                                          <database name>
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
 
-
-def main():
-    """
-    List all 'States' table objects in 'hbtn_0e_usa' database
-    """
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import create_engine
-    from sys import argv
-
-    from model_state import Base, State
-
-    connection = "mysql+mysqldb://{}:{}@localhost/{}".format(str(argv[1]),
-                                                             str(argv[2]),
-                                                             str(argv[3]))
-    engine = create_engine(connection)
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    query = session.query(State, City).\
-        filter(City.state_id == State.id).all()
-    for row in query:
-        print("{}: ({}) {}".format(row[0].name, row[1].id, row[1].name))
-
-
-if __name__ == '__main__':
-    main()
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
