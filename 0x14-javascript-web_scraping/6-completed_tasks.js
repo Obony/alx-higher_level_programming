738d369a65e0fa91ba1cdf7a200e95e220755172
Obony <faithnyabz69@gmail.com>
2023-07-25 18:45:04 +0000

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,24 +1,16 @@
 #!/usr/bin/node
-const args = require('process').argv;
 const request = require('request');
-
-const completedTasks = {};
-const url = String(args[2]);
-request(url, (err, res, body) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  body = JSON.parse(body);
-  for (const task of body) {
-    if (task.completed) {
-      if (task.userId in completedTasks) {
-        completedTasks[task.userId]++;
-      } else {
-        completedTasks[task.userId] = 1;
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
+    });
+    console.log(completed);
   }
-  console.log(completedTasks);
 });
