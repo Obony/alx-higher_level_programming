41cb281d0b2f1d035e2da86ac62c0aa8df7a4f6f
Obony <faithnyabz69@gmail.com>
2023-08-02 14:31:39 +0300

    Update Star wars character, added alternative code as a comment

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -6,3 +6,12 @@ $(document).ready(function () {
       }
     );
   });
+
+// Another way
+// document.addEventListener("DOMContentLoaded", function () {
+// 	fetch("https://swapi-api.alx-tools.com/api/people/5/?format=json")
+// 		.then((response) => response.json())
+// 		.then((data) => {
+// 			$("#character").text(data.name);
+// 		});
+// });
